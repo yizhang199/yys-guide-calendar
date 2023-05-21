fa2c22f043c7d688df6785f5514e256bcabe9976
yizhang199 <yizhang199@gmail.com>
2023-05-21 22:34:31 +1000

    add all tasks and background image into the calendar

--- src/App.js
+++ src/App.js
@@ -16,16 +16,19 @@ function App() {
   return (
     <div className="app">
       <div className="calendar">
-        {weeks.map((week) => (
+        {/* {weeks.map((week) => (
           <div key={week} className="week-header">
             {week}
           </div>
-        ))}
+        ))} */}
         {Array.from({ length: 3 }).map((_, rowIndex) => {
           return Array.from({ length: 7 }).map((_, colIndex) => {
             return (
               <div key={`row-${rowIndex}-col-${colIndex}`}>
-                <DayCell data={Guides[rowIndex * 7 + colIndex - 1]} />
+                <DayCell
+                  data={Guides[rowIndex * 7 + colIndex - 1]}
+                  day={rowIndex * 7 + colIndex - 2}
+                />
               </div>
             );
           });
--- src/components/DayCell.jsx
+++ src/components/DayCell.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 
 import styles from './DayCell.module.scss';
 
-function DayCell({ data }) {
+const dateString = '24 May, 2023, 00:00:00';
+
+function DayCell({ data, day }) {
   if (!data) return null;
 
   const { intro, tasks } = data;
 
+  const startDate = new Date(Date.parse(dateString));
+  startDate.setDate(startDate.getDate() + day);
+
+  const dayOfWeek = startDate.toLocaleString('zh', { weekday: 'long' });
+
+  const now = new Date();
+  const isToday = now.getDate() === startDate.getDate();
+
   return (
     <>
+      <div className={styles.dayOfWeek}>{dayOfWeek}</div>
       <h4 className={styles.intro}>{intro}</h4>
       <div className={styles.taskList}>
         {tasks.map((task) => (
