8beaf7d2df1283c8a366f68ce0638d99b6f33164
yizhang199 <yizhang199@gmail.com>
2023-05-23 11:11:52 +1000

    add navigation buttons in homescreen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import './App.css';
+import { useState, useRef } from 'react';
+import classNames from 'classnames';
+import styles from './App.module.scss';
 import Guides from './data/guides.json';
 import DayCell from './components/DayCell/DayCell';
 
-function App() {
+const dateString = '24 May, 2023, 00:00:00';
 
-  const days = Object.values(Guides).map((guide,index)=>{
-    return {
-      ...guide,
-      day: index + 1
-    }
-  })
+function App() {
+  const navigationRef = useRef();
+  const [activeDay, setActiveDay] = useState(() => {
+    const startDate = new Date(Date.parse(dateString));
+    const now = new Date();
+
+    if (now < startDate) return 1;
+
+    // Calculate the difference in milliseconds
+    const diffInMilliseconds = Math.abs(now - startDate);
+
+    // Convert the difference to days
+    const diffInDays = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
+
+    return diffInDays + 1;
+  });
+
+  const handleNavButtonClick = (event) => {
+    event.preventDefault();
+
+    const { day } = event.target.dataset;
+    if(Number(day) === activeDay) return;
+
+    const container = navigationRef.current;
+    const scrollLeft = event.target.offsetLeft - container.offsetLeft - 80;
+
+    container.scrollTo({
+      left: scrollLeft,
+      behavior: 'smooth'
+    });
+
+    setActiveDay(Number(day) || 1);
+  };
 
   return (
-    <div className="app">
-      <div className="days-grid">
-        {days.map(day=>{
+    <div className={styles.app}>
+      <div className={styles.navigation} ref={navigationRef}>
+        {Array.from({ length: 15 }).map((_, index) => {
+          const startDate = new Date(Date.parse(dateString));
+          startDate.setDate(startDate.getDate() + index);
+          const dayOfWeek = startDate.toLocaleString('zh', {
+            weekday: 'short',
+          });
+          const day = Guides[index + 1];
+          if (!day) return null;
           return (
-            <DayCell
-              data={day}
-            />
-          )
+            <button
+              key={`nav-button-${index}`}
+              data-day={index + 1}
+              className={classNames(styles.navButton, {[styles.active]: index + 1 === activeDay})}
+              onClick={handleNavButtonClick}
+            >
+              <small>{dayOfWeek}</small>
+              <span>{startDate.getDate().toString().padStart(2, '0')}</span>
+            </button>
+          );
         })}
       </div>
+      <DayCell data={Guides[activeDay]} day={activeDay}/>
     </div>
   );
 }
--- src/components/DayCell/DayCell.jsx
+++ src/components/DayCell/DayCell.jsx
@@ -4,10 +4,10 @@ import styles from './DayCell.module.scss';
 
 const dateString = '24 May, 2023, 00:00:00';
 
-function DayCell({ data }) {
+function DayCell({ data, day }) {
   if (!data) return null;
 
-  const { intro, tasks,  day} = data;
+  const { intro, tasks} = data;
 
   const startDate = new Date(Date.parse(dateString));
   startDate.setDate(startDate.getDate() + day - 1);
@@ -32,7 +32,7 @@ function DayCell({ data }) {
       <h4 className={styles.intro}>{intro}</h4>
       <div className={styles.taskList}>
         {tasks.map((task) => (
-          <div className={styles.task} key="task">
+          <div className={styles.task} key={task}>
             <input type="checkbox" />
             <span className={styles.taskName}>{task}</span>
           </div>
