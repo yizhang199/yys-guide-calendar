b04ec3f31a9d4f3f25e9357d15e41d3d06290bbf
yizhang199 <yizhang199@gmail.com>
2023-05-23 11:21:51 +1000

    fix route basename

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Detail from './components/Detail/Detail'
+import Detail from './components/Detail/Detail';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'detail/:day',
-    element: <Detail />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: 'detail/:day',
+      element: <Detail />,
+    },
+  ],
+  { basename: '/yys-guide-calendar' }
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
