b63683e4f3df26ba4c40ae40d0cf3236c96d607d
yizhang199 <yizhang199@gmail.com>
2023-06-04 22:41:18 +1000

    save and load checked status for the instances using store2

--- src/pages/Secret/Secret.jsx
+++ src/pages/Secret/Secret.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useSearchParams, NavLink } from 'react-router-dom';
 import classNames from 'classnames/bind';
+import store from 'store2';
+
 import { information, difficulties } from '../Secrets/Secrets.data';
+
 import styles from './Secret.module.scss';
 
 function Secret() {
@@ -30,17 +33,47 @@ function Secret() {
         X
       </NavLink>
       {filteredInstances?.map((instance) => {
-        return (
-          <div className={styles.listItem} key={instance.name}>
-            <input type="checkbox" />
-            <span className={styles.name}>{instance.name}</span>
-            <span className={styles.reward}>
-              {instance.rewardType} * {instance.rewardAmount}
-            </span>
-            <span className={styles.description}>{instance.description}</span>
-          </div>
-        );
+        return <Instance instance={instance} name={name} key={instance.name} />;
       })}
+    </div>
+  );
+}
+
+function Instance({ instance, name }) {
+  const [isDone, setDone] = useState(false);
+
+  const handleOnCheck = (event) => {
+    const { checked } = event.target;
+    const arr = store.get(name) || [];
+
+    if (checked) {
+      store.set(name, [...arr, instance.level]);
+    } else {
+      store.set(
+        name,
+        arr.filter((elem) => elem !== instance.level)
+      );
+    }
+
+    setDone(checked);
+  };
+
+  useEffect(() => {
+    const arr = store.get(name) || [];
+
+    if (arr.includes(instance.level) !== isDone) {
+      setDone(!isDone);
+    }
+  }, [name]);
+
+  return (
+    <div className={styles.listItem} key={instance.name}>
+      <input type="checkbox" onChange={handleOnCheck} checked={isDone} />
+      <span className={styles.name}>{instance.name}</span>
+      <span className={styles.reward}>
+        {instance.rewardType} * {instance.rewardAmount}
+      </span>
+      <span className={styles.description}>{instance.description}</span>
     </div>
   );
 }
