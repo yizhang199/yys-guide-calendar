f59114261850f7456c879ab7bef3a0ee8193b330
yizhang199 <yizhang199@gmail.com>
2023-06-06 21:12:35 +1000

    data entry

---
+++ src/data/heros.js
@@ -0,0 +1,8 @@
+export const Heros = {
+  1: { name: '同行待霄姑获鸟', soul: '狂骨', speed: null },
+  2: { name: '座敷童子', soul: '火灵', speed: null },
+  3: { name: '山兔', soul: '新手礼包·雪幽魂', speed: null },
+  4: { name: '系统麓铭大岳丸', soul: '', speed: '' },
+  5: { name: '系统阿修罗', soul: '', speed: '' },
+  6: { name: '神堕八岐大蛇', soul: '', speed: '' },
+};
